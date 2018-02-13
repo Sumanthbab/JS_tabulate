83e7b77ca7b3cac47796442c982dee5025dd8b2d
Sumanthbab <36447296+Sumanthbab@users.noreply.github.com>
2018-02-13 19:52:21 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express    = require("express");
+ var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : 'your_mysql_password',
+   database : 'dbname'
+ });
+ var app = express();
+ 
+ connection.connect(function(err){
+ if(!err) {
+     console.log("Database is connected ... \n\n");  
+ } else {
+     console.log("Error connecting database ... \n\n");  
+ }
+ });
