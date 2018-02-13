f3de41c45a42aac216c1045192f1115c8ae1f689
Sumanthbab <36447296+Sumanthbab@users.noreply.github.com>
2018-02-14 00:43:43 +0530

    Update reghandler.js

--- reghandler.js
+++ reghandler.js
@@ -1,1 +1,12 @@
-
+bcrypt.hash(req.body.password, 5, function( err, bcryptedPassword) {
+   
+   var users={
+     "first_name":req.body.first_name,
+     "last_name":req.body.last_name,
+     "email":req.body.email,
+     "password":bcryptedPassword,
+     "created":today,
+     "modified":today
+   }
+   ...
+  });
