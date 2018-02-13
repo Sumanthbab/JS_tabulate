69a68d6cccacecaeb268a670b7ecc06fa8d24d28
Sumanthbab <36447296+Sumanthbab@users.noreply.github.com>
2018-02-13 20:14:07 +0530

    Update register.js

--- register.js
+++ register.js
@@ -25,3 +25,15 @@ exports.register = function(req,res){
   }
   });
 }
+bcrypt.hash(req.body.password, 5, function( err, bcryptedPassword) {
+   
+   var users={
+     "first_name":req.body.first_name,
+     "last_name":req.body.last_name,
+     "email":req.body.email,
+     "password":bcryptedPassword,
+     "created":today,
+     "modified":today
+   }
+   ...
+  });
